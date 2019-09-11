e015f8ccb13430e539cf7b93496530fb2bedd94f
Farrell0828 <mengfanqi0828@163.com>
2019-09-11 12:46:32 +0800

    Runable oof_eval.

--- src/oof_eval.py
+++ src/oof_eval.py
@@ -8,7 +8,7 @@ from process import postprocess
 from dataset import DataGenerator 
 from utils import dice_coef_score 
 
-os.environ['CUDA_VISIBLE_DEVICES'] = ''
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument(
@@ -27,19 +27,20 @@ def _main_():
     oof_true_masks = []
     for i in range(5):
         config['train']['fold'] = i
-        generator = DataGenerator(config=config, 
+        generator = DataGenerator(config=config['train'], 
                                   preprocessing=sm_model.preprocessing,
                                   n_class=sm_model.n_class, 
                                   split='val', 
                                   full_size_mask=True)
-        weithts_path = os.path.join(config['save_model_folder'], 
+        weithts_path = os.path.join(config['train']['save_model_folder'], 
                                     'val_best_fold_{}_weights.h5'.format(i))
         sm_model.model.load_weights(weithts_path)
-        print('Fold {} eval begin.')
+        print('Fold {} eval begin.'.format(i))
         for X, y in tqdm(list(generator)):
             y_preds = sm_model.model.predict(X)
             y_preds = postprocess(y_preds, config['postprocess'], True)
             oof_preds.append(y_preds)
+            y = y[:, :, :, :4]
             oof_true_masks.append(y)
     oof_preds = np.concatenate(oof_preds)
     oof_true_masks = np.concatenate(oof_true_masks)
@@ -47,3 +48,5 @@ def _main_():
     cv_dice_coef = dice_coef_score(oof_true_masks, oof_preds)
     print('CV Dice Coef Score: {}'.format(cv_dice_coef))
 
+if __name__ == '__main__':
+    _main_()
