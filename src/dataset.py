99bf662dee5e7a02ca79320af940b4421bf37da4
Farrell Meng <mengfanqi0828@163.com>
2019-09-28 01:14:12 +0800

    Add DataGenerator4Cls, not test yet.

--- src/dataset.py
+++ src/dataset.py
@@ -153,6 +153,104 @@ class DataGenerator(Sequence):
         self.aug = albu.Compose(aug, p=1, additional_targets=additional_targets)
 
 
+class DataGenerator4Cls(Sequence):
+    def __init__(self, 
+                 config, 
+                 preprocessing=None,
+                 n_class=5,
+                 split='train', 
+                 random_seed=606,
+                 shuffle=False):
+
+        super().__init__()
+        self.preprocessing = preprocessing
+        self.n_class = n_class
+        self.split = split
+        self.shuffle = shuffle
+        self.data_folder = config['images_folder']
+        self.height = config['image_height']
+        self.width = config['image_width']
+        self.batch_size = config['batch_size']
+        self.aug_pipline = config['aug_pipline']
+        if config['aug_pipline'] != []:
+            self.build_aug()
+        
+        if split != 'test':
+            self.fold = config['fold']
+            df = pd.read_csv(config['csv_file_path'])
+            skf = StratifiedKFold(n_splits=5, random_state=random_seed, shuffle=True)
+            train_idx, val_idx = list(skf.split(df, df['MaskCount']))[self.fold]
+            if split == 'train':
+                self.df = df.iloc[train_idx]
+            elif split == 'val':
+                self.df = df.iloc[val_idx]
+            else:
+                raise ValueError("Split '{}' not support now.".format(split))
+            self.n_samples = len(self.df)
+        else:
+            self.image_file_paths = glob(self.data_folder + '/*')
+            self.n_samples = len(self.image_file_paths)
+
+        self.on_epoch_end()
+
+    def __len__(self):
+        return int(np.ceil(self.n_samples / self.batch_size))
+    
+    def on_epoch_end(self):
+        self.indexes = np.arange(self.n_samples)
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+    
+    def __getitem__(self, index): 
+        indexes = self.indexes[index*self.batch_size : (index+1)*self.batch_size]
+        X = np.empty((len(indexes), self.height, self.width, 3), dtype=np.float32)
+
+        if self.split == 'test':
+            filenames = []
+            for i, file_name in enumerate([self.image_file_paths[index] for index in indexes]):
+                X[i, ] = Image.open(file_name).resize((self.width, self.height))
+                filenames.append(file_name.split('/')[-1])
+        elif self.split in ['train', 'val']:
+            for i, file_name in enumerate(self.df['ImageId'].iloc[indexes]):
+                X[i, ] = Image.open(os.path.join(self.data_folder, file_name)).resize((self.width, self.height))
+                if self.aug_pipline != []:
+                    augmented = self.aug(image=X[i, ])
+                    X[i, ] = augmented['image']
+            if self.n_class == 5:
+                y = self.df[['dt_0', 'dt_1', 'dt_2', 'dt_3', 'dt_4']].iloc[indexes].values
+            elif self.n_class == 2:
+                y = (self.df['MaskCount'].iloc[indexes] > 0).values.astype(np.uint8)
+        else:
+            raise ValueError('Split {} not support.'.format(self.split))
+
+        if self.preprocessing is not None: X = self.preprocessing(X)
+        
+        if self.split != 'test': 
+            return X, y
+        else:
+            return X, filenames
+    
+    def build_aug(self):
+        aug = []
+        for aug_type in self.aug_pipline:
+            if aug_type == 'Flip':
+                aug += [albu.HorizontalFlip(), albu.VerticalFlip()]
+            elif aug_type == 'Non-Spatial':
+                aug.append(albu.OneOf([
+                    albu.RandomContrast(),
+                    albu.RandomGamma(),
+                    albu.RandomBrightness(),
+                ], p=0.3))
+            elif aug_type == "Non-Rigid":
+                aug.append(albu.OneOf([
+                    albu.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
+                    albu.GridDistortion(),
+                    albu.OpticalDistortion(distort_limit=2, shift_limit=0.5),
+                ], p=0.3))
+            else:
+                raise ValueError('aug type {} not support.'.format(aug_type))
+        self.aug = albu.Compose(aug, p=1)
+
 if __name__ == '__main__':
     config_path = './configs/config_mibook.json'
     with open(config_path) as config_buffer:    
@@ -163,8 +261,4 @@ if __name__ == '__main__':
     print(X.shape, X.dtype)
     print(y.shape, y.dtype)
     img = Image.fromarray(X[0].astype(np.uint8))
-    masks = [Image.fromarray(y[0, :, :, i]*255) for i in range(5)]
-    img.show(title='image')
-    for i in range(5): 
-        masks[i].show(title='mask[{}]'.format(i))
-
+    print(y)
