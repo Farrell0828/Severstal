0236c3a4755b00f877ce2f64529732fe20ae6a16
Farrell0828 <mengfanqi0828@163.com>
2019-10-01 20:39:51 +0800

    Optimize the efficience.

--- src/postprocess_search.py
+++ src/postprocess_search.py
@@ -17,7 +17,7 @@ argparser.add_argument(
     help='Config file.'
 )
 
-def oof_eval(sm_model, config):
+def oof_predict(sm_model, config):
     oof_preds = []
     oof_true_masks = []
     for i in range(5):
@@ -33,36 +33,46 @@ def oof_eval(sm_model, config):
         print('Fold {} eval begin.'.format(i))
         for X, y in tqdm(list(generator)):
             y_preds = sm_model.model.predict(X)
-            y_preds = postprocess(y_preds, config['postprocess'], True)
             oof_preds.append(y_preds)
             y = y[:, :, :, :4]
             oof_true_masks.append(y)
     oof_preds = np.concatenate(oof_preds)
     oof_true_masks = np.concatenate(oof_true_masks)
-    
+    return oof_preds, oof_true_masks
+
+def oof_eval(oof_preds, oof_true_masks, config):
+    oof_preds = postprocess(oof_preds, config['postprocess'], True)
     cv_dice_coef = dice_coef_score(oof_true_masks, oof_preds)
     print('CV Dice Coef Score: {}'.format(cv_dice_coef))
     return cv_dice_coef
 
-def _main_():
+def search_min_size(oof_preds, oof_true_masks, n):
+    best_score = 0
+    best_config = config['postprocess']
+    min_size_cadidates = [2**(n+5), 2**(n+6), 2**(n+7), 2**(n+8), 2**(n+9)]
+    print('min_size Cadidates: ', min_size_cadidates)
+    for min_size in min_size_cadidates:
+        config['postprocess']['min_size'] = [1024, 1024, 1024, 1024]
+        config['postprocess']['min_size'][n-1] = min_size
+        print('min_size this round: ', config['postprocess']['min_size'])
+        score = oof_eval(oof_preds, oof_true_masks, config)
+        if score > best_score:
+            best_config = config['postprocess'].copy()
+            best_score = score
+        print()
+    print('Best score: ', best_score)
+    print('Best score config: ', best_config)
+    print()
+
+if __name__ == '__main__':
     args = argparser.parse_args()
     config_path = args.config
     with open(config_path) as config_buffer:
         config = json.loads(config_buffer.read())
+        
     sm_model = SMModel(config['model'])
-    best_score = 0
-    best_config = config['postprocess']
-    for min_size_1 in [64, 128, 256, 512, 1024]:
-        min_size_2 = min_size_1 * 2
-        min_size_3 = min_size_1 * 4
-        min_size_4 = min_size_1 * 8
-        config['postprocess']['min_size'] = [min_size_1, min_size_2, min_size_3, min_size_4]
-        score = oof_eval(sm_model, config)
-        if score > best_score:
-            best_config = config['postprocess'].copy()
-            best_score = score
-    print(best_score)
-    print(best_config)
+    oof_preds, oof_true_masks = oof_predict(sm_model, config)
 
-if __name__ == '__main__':
-    _main_()
+    for i in range(4):
+        print('Search for class {} begin...'.format(i+1))
+        search_min_size(oof_preds, oof_true_masks, i+1)
