8275610a1ec07b82e0e925b6aeedb0a7abb648b9
Farrell0828 <mengfanqi0828@163.com>
2019-10-01 20:37:57 +0800

    Add fold param. Make it could train all folds in sigle run.

--- src/train.py
+++ src/train.py
@@ -12,12 +12,25 @@ argparser.add_argument(
     help='Config file.'
 )
 
+argparser.add_argument(
+    '-f',
+    '--fold',
+    help='Which fold to train.'
+)
+
 if __name__ == '__main__':
 
     args = argparser.parse_args()
     config_path = args.config
     with open(config_path) as config_buffer:    
         config = json.loads(config_buffer.read())
-
-    sm_model = SMModel(config['model'])
-    sm_model.train(config['train'])
+    fold = int(args.fold)
+    if fold == -1:
+        for i in range(5):
+            config['train']['fold'] = i
+            sm_model = SMModel(config['model'])
+            sm_model.train(config['train'])
+    else:
+        config['train']['fold'] = fold
+        sm_model = SMModel(config['model'])
+        sm_model.train(config['train'])
